ffcf2f0dfe52459407ba11dea39f97079de55b6b
jkenjayev <javohirwebdev@gmail.com>
2022-05-17 17:49:58 +0500

    Products are displayed with card

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const Laptop = require("../models/Laptop");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("product/index", {title: "Laptops", isProducts: true});
+router.get("/", async (req, res) => {
+  const laptops = await Laptop.getAllData();
+  res.render("product/index", { title: "Laptops", isProducts: true, laptops });
 });
 
 module.exports = router;
