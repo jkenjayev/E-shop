7978708c075b45d8d2d687954499debd98509468
jkenjayev <javohirwebdev@gmail.com>
2022-05-20 10:26:56 +0500

    Laptop edit page is created but Create laptop page has a bug

--- routes/products.js
+++ routes/products.js
@@ -7,9 +7,17 @@ router.get("/", async (req, res) => {
   res.render("products/index", { title: "Laptops", isProducts: true, laptops });
 });
 
+router.get("/:id/edit", async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect("/");
+  }
+  const laptop = await Laptop.getLaptopById(req.params.id);
+  res.render("laptops/update", { laptop });
+});
+
 router.get("/:id", async (req, res) => {
   const laptop = await Laptop.getLaptopById(req.params.id);
-  res.render("laptops/laptop", {layout: "details", laptop });
+  res.render("laptops/laptop", { layout: "details", laptop });
 });
 
 module.exports = router;
