10dc4d6e3afa5da603a344f82979f8dcc6da0474
jkenjayev <javohirwebdev@gmail.com>
2022-05-27 23:13:04 +0500

    mongoose added and connection with mongodb is created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const hbs = require("./dependencies/engine-config");
 const app = express();
 /* Routers */
@@ -6,6 +7,9 @@ const HomeRoute = require("./routes/home");
 const ProductsRoute = require("./routes/products");
 const CreateProductRoute = require("./routes/createProduct");
 const AddCardRoute = require("./routes/AddCard");
+
+const startProject = start();
+
 /* App sets */
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
@@ -20,5 +24,17 @@ app.use("/", HomeRoute);
 app.use("/products", ProductsRoute);
 app.use("/products/create", CreateProductRoute);
 app.use("/card", AddCardRoute);
-/* Listener */
-app.listen(5000, () => console.log(`Server has been running on port 5000`));
+
+/* Database connection*/
+async function start() {
+  try {
+    const url =
+      "mongodb+srv://john:123@cluster0.clcgx.mongodb.net/?retryWrites=true&w=majority";
+    await mongoose.connect(url, { useNewUrlParser: true });
+
+    /* Listener */
+    app.listen(5000, () => console.log(`Server has been running on port 5000`));
+  } catch (e) {
+    console.log(e);
+  }
+}
