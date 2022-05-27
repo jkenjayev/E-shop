8d828627694bf105728129d0a98a1e96c7159259
jkenjayev <javohirwebdev@gmail.com>
2022-05-27 23:29:05 +0500

    Laptop model changed to mogoose based model

--- models/Laptop.js
+++ models/Laptop.js
@@ -1,79 +1,25 @@
-const { v4: uuidv4 } = require("uuid");
-const path = require("path");
-const fs = require("fs");
+const { Schema, model } = require("mongoose");
 
-class Laptop {
-  constructor(title, price, description, img) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.img = img;
-    this.id = uuidv4();
-  }
+const laptops = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
 
-  jsonDataToString() {
-    return {
-      title: this.title,
-      price: this.price,
-      description: this.description,
-      img: this.img,
-      id: this.id,
-    };
-  }
+  price: {
+    type: Number,
+    required: true,
+  },
 
-  async save() {
-    const laptops = await Laptop.getAllData();
-    laptops.push(this.jsonDataToString());
+  img: {
+    type: String,
+    required: true,
+  },
 
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, "..", "data", "laptop.json"),
-        JSON.stringify(laptops),
-        (err) => {
-          if (err) reject(err);
-          resolve();
-        }
-      );
-    });
-  }
+  description: {
+    type: String,
+    required: true,
+  },
+});
 
-  static getAllData() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(
-        path.join(__dirname, "..", "data", "laptop.json"),
-        "utf-8",
-        (err, data) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(data));
-          }
-        }
-      );
-    });
-  }
-
-  static async getLaptopById(id) {
-    const laptop = await Laptop.getAllData();
-    return laptop.find(l => l.id === id);
-  }
-
-  static async update(laptop) {
-    const laptops = await Laptop.getAllData();
-    const index = laptops.findIndex(l => l.id === laptop.id) ;
-    laptops[index] = laptop;
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, "..", "data", "laptop.json"),
-        JSON.stringify(laptops),
-        (err) => {
-          if (err) reject(err);
-          resolve();
-        }
-      );
-    });
-  }
-}
-
-module.exports = Laptop;
+module.exports = model("Laptop", laptops);
